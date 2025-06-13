b64fb5a1ac04525046a05ed41e613da7170185e2
Udaykumar725 <udaykumar77725@gmail.com>
2025-06-13 12:20:18 +0530

    client setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-600">Hello world!</h1>
+       <Header/>
+      <h1 className="text-3xl font-bold text-black-600">Mithila Bazar</h1>
+      <Footer/>
     </>
   );
 }
