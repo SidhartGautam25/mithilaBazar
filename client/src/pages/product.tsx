977ad0ddc8492df4de02a8f294b39d3a77215f93
Navneet Nishchal <96915641+SidhartGautam25@users.noreply.github.com>
2025-06-15 21:16:51 +0530

    Revert "product page also completed for now"

--- client/src/pages/product.tsx
+++ client/src/pages/product.tsx
@@ -1,22 +1,7 @@
 import React from "react";
-import ProductImages from "../comp/product/ProductImages";
-import ProductInfo from "../comp/product/ProductInfo";
-import RelatedItems from "../comp/product/RelatedItems";
-import Navbar from "../comp/navbar/Navbar";
-import Footer from "../comp/footer/Footer";
 
 const Product: React.FC = () => {
-  return (
-    <>
-      <Navbar />
-      <div className="flex flex-col md:flex-row max-w-7xl mx-auto px-4 py-8">
-        <ProductImages />
-        <ProductInfo />
-      </div>
-      <RelatedItems />
-      <Footer />
-    </>
-  );
+  return <div>Product</div>;
 };
 
 export default Product;
